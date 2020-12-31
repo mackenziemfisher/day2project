b4939dce60b774de723691275bfe69867b4b7290
mackenziemfisher <66263985+mackenziemfisher@users.noreply.github.com>
2020-12-31 01:54:19 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,35 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import {useState} from 'react';
+import Game from '../components/Game'
+import Mainmenu from '../components/Mainmenu'
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+const Home = () => {
+    const start = () => {
+        console.log(1)
+;    };
+    const [highscore, setHighscore] = useState(0);
+    const [playing, setPlaying] = useState(false);
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+    const startGame = () => {
+        setPlaying(true);
+    };
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    const gameOver = (score) => {
+        if(score > highscore){
+            setHighscore(score)
+        }
+        setPlaying(false);
+    };
+    
+    return(
+        <div className="flex flex-col flex-center justify-center h-screen bg-green-100">
+        <div className = "flex flex-col text-center items-center">
+            Math is Math Multiplication Game
+            <Mainmenu onStart={start}/>
+            <img class="image-center" src="mathislogo.png" />
+            </div>
         </div>
-      </main>
+    );
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
-}
+};
+
+export default Home;
